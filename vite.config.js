5759f3a27446ee1d2a4499b5c7616b6f9b92f38a
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-07-09 16:03:07 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'vinicius05051993/vue-catalog',
+  base: 'vue-catalog',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
