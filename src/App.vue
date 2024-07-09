51f01776f920bb14cc3e09a89e1f260dc2fcd2fc
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-07-09 15:09:16 -0300

    test

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it Commit!" />
+      <HelloWorld msg="You did it Commit!!!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
